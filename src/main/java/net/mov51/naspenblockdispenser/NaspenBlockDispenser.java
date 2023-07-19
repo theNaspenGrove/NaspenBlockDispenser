17b6449e18f5d25e2097bb5ee6c3351a151d617e
mov <mov51@mov51.net>
2023-07-19 00:48:52 -0400

    Add tnt block destruction

--- src/main/java/net/mov51/naspenblockdispenser/NaspenBlockDispenser.java
+++ src/main/java/net/mov51/naspenblockdispenser/NaspenBlockDispenser.java
@@ -1,12 +1,16 @@
 package net.mov51.naspenblockdispenser;
 
 import net.mov51.naspenblockdispenser.listeners.BlockDispenseEventListener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NaspenBlockDispenser extends JavaPlugin {
 
+    public static Plugin plugin;
+
     @Override
     public void onEnable() {
+        plugin = this;
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(new BlockDispenseEventListener(), this);
         getLogger().info("Dispense your blocks!");
--- src/main/java/net/mov51/naspenblockdispenser/listeners/BlockDispenseEventListener.java
+++ src/main/java/net/mov51/naspenblockdispenser/listeners/BlockDispenseEventListener.java
@@ -1,13 +1,17 @@
 package net.mov51.naspenblockdispenser.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.Container;
 import org.bukkit.block.Dispenser;
 import org.bukkit.block.data.Directional;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.inventory.ItemStack;
+
+import static net.mov51.naspenblockdispenser.NaspenBlockDispenser.plugin;
 
 public class BlockDispenseEventListener implements Listener {
     @EventHandler
@@ -16,14 +20,28 @@ public class BlockDispenseEventListener implements Listener {
             if(event.getItem().getType() == Material.SHULKER_BOX){
                 return;
             }
-            if(event.getItem().getType() == Material.TNT){
-                return;
-            }
             if(event.getItem().getType().isBlock()){
                 Block placeAtBlock = event.getBlock().getWorld().getBlockAt(event.getBlock().getLocation().add(((Directional) event.getBlock().getBlockData()).getFacing().getDirection()));
+                if(event.getItem().getType() == Material.TNT){
+                    if (!placeAtBlock.getType().isAir()) {
+                        event.setCancelled(true);
+                        placeAtBlock.breakNaturally();
+                        Dispenser dispenser = (Dispenser) event.getBlock().getState();
+                        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                            dispenser.getInventory().removeItem(event.getItem());
+                            dispenser.update();
+                        }, 1L);
+                    }
+                    return;
+                }
                 if(placeAtBlock.getType().isAir() && !containsEntity(placeAtBlock)){
                     placeAtBlock.setType(event.getItem().getType());
-                    event.setItem(new ItemStack(org.bukkit.Material.AIR));
+                    Dispenser dispenser = (Dispenser) event.getBlock().getState();
+                    event.setCancelled(true);
+                    Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                        dispenser.getInventory().removeItem(event.getItem());
+                        dispenser.update();
+                    }, 1L);
                 }
             }
         }
